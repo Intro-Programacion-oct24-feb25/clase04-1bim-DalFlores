a7db3d2088e713551c3c7805940f2b7aeb696569
Dalton Flores <daltonflores@gmail.com>
2024-10-30 11:54:01 -0500

    Cambios

---
+++ Ejemplo01/src/ejemplo01/Ejemplo10.java
@@ -0,0 +1,23 @@
+/*
+René Rolando
+
+Elizalde Solano
+
+1983
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo10 {
+    public static void main(String[] args) {
+        // Secuencia de escape \n y \t
+        String nombreEstudiante = "Dalton Smit";
+        String apellidoEstudiante = "Flores Rodriguez";
+        int edad = 18;
+        
+        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante+"\n\t\t"+edad);    
+    }
+}
